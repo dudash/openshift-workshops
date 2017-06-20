96b42e5ff8dac61981dd28d85f78aaec6858d162
jottofar <jottofar@redhat.com>
2017-06-20 11:54:00 -0400

    jottofar changes (#22)
    jottofar PR

--- dc-metro-map/routes/index.js
+++ dc-metro-map/routes/index.js
@@ -5,7 +5,7 @@ var rest = require('restler');
 //-----------------------------------------------------------------------------
 // CONSTANTS AND HELPERS
 //-----------------------------------------------------------------------------
-var API_KEY_PLACEHOLDER = process.env.WMATA_API_KEY || '6b700f7ea9db408e9745c207da7ca827';
+var API_KEY_PLACEHOLDER = process.env.WMATA_API_KEY || 'e1eee2b5677f408da40af8480a5fd5a8';
 var BEERME = process.env.BEERME || false;
 var RAINBOW = process.env.RAINBOW || false;
 console.log("using WMATA API Key - " + API_KEY_PLACEHOLDER);
